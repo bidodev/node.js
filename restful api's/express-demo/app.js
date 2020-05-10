d79abe68a30261518d101c90b181d448a5323a80
Claudinei Bido <bido.c@live.com>
2020-05-10 21:36:41 +0200

    route parameters

--- restful api's/express-demo/app.js
+++ restful api's/express-demo/app.js
@@ -17,6 +17,12 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000...");
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on the port ${port}`);
 });
