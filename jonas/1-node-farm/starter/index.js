85701ed794c41388a90f514f81946d3f2f55b204
Claudinei Bido <bido.c@live.com>
2020-05-25 15:45:24 +0200

    wraping node farm

--- jonas/1-node-farm/starter/index.js
+++ jonas/1-node-farm/starter/index.js
@@ -23,18 +23,62 @@ const url = require("url");
 //     });
 //   });
 // });
+
+const replace = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
 //top level code..
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const parsed = JSON.parse(data);
 
 //SERVER
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/overview") {
-    res.end("This is the overview..");
-  } else if (url === "/product") {
-    res.end("This is the product");
-  } else if (url === "/api") {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cards = parsed.map(element => replace(tempCard, element)).join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cards);
+    res.end(output);
+  }
+
+  //product page
+  else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = parsed[query.id];
+    const output = replace(tempProduct, product);
+
+    res.end(output);
+  }
+  //this is the api
+  else if (pathname === "/api") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(data);
   } else {
