9b2bad71e15c722623c10925a5d8e05ea9a7fcea
Claudinei Bido <bido.c@live.com>
2020-05-25 11:52:38 +0200

    daily exercise

--- jonas/1-node-farm/starter/ass.js
+++ jonas/1-node-farm/starter/ass.js
@@ -2,10 +2,7 @@ const args = process.argv.slice(2);
 
 (genOutput = args => {
   const star = "*";
-
-  if (!args.length) {
-    args = [10, "Hey"];
-  }
+  !args.length ? (args = [10, "Hey"]) : args;
 
   console.log(`${star.repeat(args[0])}\n${args[1]}\n${star.repeat(args[0])}`);
 })(args);
