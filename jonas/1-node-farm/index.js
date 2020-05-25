d02040ca029cf5b3e505b5c7f203a251e9087277
Claudinei Bido <bido.c@live.com>
2020-05-25 15:57:29 +0200

    node-js course / dci

--- jonas/1-node-farm/index.js
+++ jonas/1-node-farm/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replace = require("./modules/replace");
 
 /////SYNC (blocking code)
 //reading text from files
@@ -23,22 +24,6 @@ const url = require("url");
 //     });
 //   });
 // });
-
-const replace = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
 
 //top level code..
 const tempOverview = fs.readFileSync(
@@ -81,7 +66,10 @@ const server = http.createServer((req, res) => {
   else if (pathname === "/api") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(data);
-  } else {
+  }
+
+  //404
+  else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<h1>Page not found</h1>");
   }
