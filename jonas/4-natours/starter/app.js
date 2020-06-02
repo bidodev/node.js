2039ab4e35320e7d362705516a115309ab16a95c
Claudinei Bido <bido.c@live.com>
2020-06-02 15:34:54 +0200

    get tours

--- jonas/4-natours/starter/app.js
+++ jonas/4-natours/starter/app.js
@@ -1,18 +1,29 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-const port = process.env.port || 3000;
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', app: 'Natours' });
+// });
 
-app.get('/', (req, res) => {
+// // POST method route
+// app.post('/', (req, res) => {
+//   res.send('POST request to the homepage');
+// });
+//read tours data
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
-    .json({ message: 'Hello from the server side', app: 'Natours' });
+    .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
-// POST method route
-app.post('/', (req, res) => {
-  res.send('POST request to the homepage');
-});
+const port = process.env.port || 3000;
 
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`)
